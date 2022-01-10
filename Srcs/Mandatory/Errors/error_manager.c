94860ebf936abdf07f68f05b603ca713379e4dde
Albe MARINI-AUDOUARD <amarini-@e2r8p20.clusters.42paris.fr>
2022-01-10 18:33:33 +0100

    :construction: :recycle: - Working on error manager

---
+++ Srcs/Mandatory/Errors/error_manager.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   error_manager.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amarini- <amarini-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/10 18:31:41 by amarini-          #+#    #+#             */
+/*   Updated: 2022/01/10 18:33:20 by amarini-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+
+#ifndef BONUS
+int	error_manager(int erno)
+{
+	char	*error;
+	//get error depending on error
+	//
+}
+
+#else
+
+
+#endif
